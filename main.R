581c1d3b7c335476f740f2bbec9137be720d1f68
vkarhune <vkarhune@gmail.com>
2021-01-17 10:34:24 +0000

    Initiate

---
+++ main.R
@@ -0,0 +1,11 @@
+# reading:
+
+# colocalization
+# Pickrell, Giambartolomei, Wallace (>1 causal SNPs)
+
+# different MR methods:
+# https://www.nature.com/articles/s41467-020-18716-x
+
+
+# others: Magma, LAVA
+# https://www.biorxiv.org/content/10.1101/2020.12.31.424652v1
